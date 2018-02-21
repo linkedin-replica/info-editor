c78d968d0b93867420110d50cc1a9a1cf0e6c405
loay elzobaidy <loayelzobeidy@gmail.com>
2018-02-21 20:49:32 +0200

    maven added

---
+++ src/main/java/database/ArangoHandler.java
@@ -0,0 +1,30 @@
+package database;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+import com.arangodb.ArangoCollection;
+import com.arangodb.ArangoCursor;
+import com.arangodb.ArangoDB;
+import com.arangodb.ArangoDBException;
+import com.arangodb.entity.BaseDocument;
+import com.arangodb.entity.CollectionEntity;
+import com.arangodb.model.AqlQueryOptions;
+import com.arangodb.util.MapBuilder;
+import com.arangodb.velocypack.VPackSlice;
+import com.arangodb.velocypack.exception.VPackException;
+public class ArangoHandler implements DatabaseHandler{
+
+
+    public void connect() {
+        // TODO
+    }
+
+  
+    public void disconnect() {
+        // TODO
+    }
+    public static void main(String [] srgs){
+
+    }
+}
---
+++ src/main/java/database/DatabaseHandler.java
@@ -0,0 +1,12 @@
+package database;
+public interface DatabaseHandler {
+    /**
+     * Initiate a connection with the database
+     */
+    void connect();
+
+    /**
+     * Send a new notification to the user
+     */
+    void disconnect();
+}
