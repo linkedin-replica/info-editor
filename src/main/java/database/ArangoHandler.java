1aa8e9cab7d8afe7cf78ccaf48183298c4b6a197
loay elzobaidy <loayelzobeidy@gmail.com>
2018-02-21 21:07:07 +0200

    dev trial

--- src/main/java/database/ArangoHandler.java
+++ src/main/java/database/ArangoHandler.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 
 import com.arangodb.ArangoCollection;
 import com.arangodb.ArangoCursor;
-import com.arangodb.ArangoDB;
+//import com.arangodb.ArangoDB;
 import com.arangodb.ArangoDBException;
 import com.arangodb.entity.BaseDocument;
 import com.arangodb.entity.CollectionEntity;
