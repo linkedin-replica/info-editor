b40a669e19ef9952daacbd36c0cddb3eaa1e051c
loay elzobaidy <loayelzobeidy@gmail.com>
2018-02-21 23:55:14 +0200

    getUser imlemented in arangodb

--- src/main/java/database/ArangoHandler.java
+++ src/main/java/database/ArangoHandler.java
@@ -2,10 +2,11 @@ package database;
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
-
+import models.User;
 import com.arangodb.ArangoCollection;
 import com.arangodb.ArangoCursor;
 //import com.arangodb.ArangoDB;
+import com.arangodb.ArangoDB;
 import com.arangodb.ArangoDBException;
 import com.arangodb.entity.BaseDocument;
 import com.arangodb.entity.CollectionEntity;
@@ -14,15 +15,20 @@ import com.arangodb.util.MapBuilder;
 import com.arangodb.velocypack.VPackSlice;
 import com.arangodb.velocypack.exception.VPackException;
 public class ArangoHandler implements DatabaseHandler{
-
+ArangoDB arangoDB;
 
     public void connect() {
         // TODO
+        arangoDB = new ArangoDB.Builder().build();
     }
 
   
     public void disconnect() {
         // TODO
+    }
+    public User getUser(){
+        User user = arangoDB.db("myDatabase").collection("myCollection").getDocument("", User.class);
+        return user;
     }
     public static void main(String [] srgs){
 
