7de5938d4c57fcad5032e3928fcbd001c73c8c2a
loay elzobaidy <loayelzobeidy@gmail.com>
2018-03-21 00:53:49 +0200

    rabbitMQ structure for testing and the the queue done

---
+++ src/test/java/ClientMessagesTest.java
@@ -0,0 +1,104 @@
+import com.arangodb.ArangoDatabase;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.linkedin.replica.editInfo.config.Configuration;
+import com.linkedin.replica.editInfo.database.DatabaseConnection;
+import com.linkedin.replica.editInfo.database.handlers.impl.ArangoEditInfoHandler;
+import com.linkedin.replica.editInfo.messaging.ClientMessagesReceiver;
+import com.linkedin.replica.editInfo.models.*;
+import com.rabbitmq.client.*;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.UUID;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeoutException;
+
+import static org.junit.Assert.assertEquals;
+
+public class ClientMessagesTest {
+    private static Configuration config;
+    private static String QUEUE_NAME;
+    private static ClientMessagesReceiver messagesReceiver;
+    private static ArangoDatabase arangoDb;
+    private static ArangoEditInfoHandler arangoHandler;
+
+    private static ConnectionFactory factory;
+    private static Connection connection;
+    private static Channel channel;
+
+    @BeforeClass
+    public static void init() throws IOException, TimeoutException {
+        String rootFolder = "src/main/resources/config/";
+        Configuration.init(rootFolder + "app.config",
+                rootFolder + "arango.test.config",
+                rootFolder + "commands.config");
+        DatabaseConnection.init();
+        config = Configuration.getInstance();
+
+        // init message receiver
+        QUEUE_NAME = config.getAppConfig("rabbitmq.queue.client");
+        messagesReceiver = new ClientMessagesReceiver();
+
+        // init db
+        arangoDb = DatabaseConnection.getInstance().getArangoDriver().db(
+                Configuration.getInstance().getArangoConfig("db.name")
+        );
+
+        arangoDb.createCollection(
+                config.getArangoConfig("collection.companies.name")
+        );
+
+        arangoHandler = new ArangoEditInfoHandler();
+
+        factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        connection = factory.newConnection();
+        channel = connection.createChannel();
+    }
+
+    @Test
+    public void testMessages() throws IOException, InterruptedException {
+        JsonObject object = new JsonObject();
+        object.addProperty("commandName", "company.get");
+        byte[] message = object.toString().getBytes();
+        final String corrId = UUID.randomUUID().toString();
+
+        String replyQueueName = channel.queueDeclare().getQueue();
+
+        AMQP.BasicProperties props = new AMQP.BasicProperties
+                .Builder()
+                .correlationId(corrId)
+                .replyTo(replyQueueName)
+                .build();
+
+        channel.basicPublish("", QUEUE_NAME, props, message);
+
+        final BlockingQueue<String> response = new ArrayBlockingQueue<String>(1);
+
+        channel.basicConsume(replyQueueName, true, new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                if (properties.getCorrelationId().equals(corrId)) {
+                    response.offer(new String(body));
+                }
+            }
+        });
+    }
+    @AfterClass
+    public static void clean() throws IOException, TimeoutException {
+        // close message queue connection
+        messagesReceiver.closeConnection();
+        channel.close();
+        connection.close();
+        // clean db
+        arangoDb.collection(
+                config.getArangoConfig("collection.companies.name")
+        ).drop();
+        DatabaseConnection.getInstance().closeConnections();
+    }
+}
