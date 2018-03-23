6f511289e98e08bd34d0420927a8180640cceadd
loay elzobaidy <loayelzobeidy@gmail.com>
2018-03-23 12:59:43 +0200

    edit user test modified

--- src/test/java/core/EditUserProfileCommandTest.java
+++ src/test/java/core/EditUserProfileCommandTest.java
@@ -1,6 +1,7 @@
 package core;
 
 import com.arangodb.ArangoDatabase;
+import com.linkedin.replica.editInfo.commands.impl.EditProfileDetailsCommand;
 import com.linkedin.replica.editInfo.database.DatabaseSeed;
 import com.linkedin.replica.editInfo.database.handlers.impl.ArangoEditInfoHandler;
 import com.linkedin.replica.editInfo.database.handlers.impl.ArangoEditInfoHandler;
@@ -47,17 +48,19 @@ public class EditUserProfileCommandTest {
     public void execute() throws IOException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
         HashMap<String, Object> args = new HashMap();
         Object response;
-        args.put("userId", "110265");
+        args.put("userId", "0");
         args.put("firstName", "Baher");
         args.put("headline", "Graduate");
-        args.put("numConnections","7");
+        Command command2 = new EditProfileDetailsCommand(args);
+        command2.setDbHandler(arangoHandler);
+        command2.execute();
         command = new GetUserProfileCommand(args);
         command.setDbHandler(arangoHandler);
         response = command.execute();
         User myUser = (User) response;
-        assertEquals("Expected matching first name", "Bebo" , myUser.getFirstName());
-        assertEquals("Expected matching last name", "Abdelmalek" , myUser.getLastName());
-        assertEquals("Expected matching headline", "Student" , myUser.getHeadline());
+        assertEquals("Expected matching first name", "Baher" , myUser.getFirstName());
+        assertEquals("Expected matching last name", "Radwan" , myUser.getLastName());
+        assertEquals("Expected matching headline", "Graduate" , myUser.getHeadline());
     }
     @AfterClass
     public static void teardown() throws IOException {
