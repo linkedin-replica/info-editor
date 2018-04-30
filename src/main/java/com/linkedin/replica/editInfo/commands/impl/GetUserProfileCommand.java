e011f411bed97c198575069de5f386e201102294
Soliman <ahmed.soliman9595@gmail.com>
2018-04-30 21:45:15 +0200

    Refactor get profile command with new data models.

--- src/main/java/com/linkedin/replica/editInfo/commands/impl/GetUserProfileCommand.java
+++ src/main/java/com/linkedin/replica/editInfo/commands/impl/GetUserProfileCommand.java
@@ -10,35 +10,25 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class GetUserProfileCommand extends Command {
-    private CacheEditInfoHandler cacheeditInfoHandler;
+    private CacheEditInfoHandler cacheEditInfoHandler;
 
     public GetUserProfileCommand(HashMap<String, Object> args) {
         super(args);
     }
 
     public Object execute() throws IOException {
-        // validate that all required arguments are passed
         validateArgs(new String[]{"userId"});
-        String[] ids = new String[1];
-        ids[0] = (String) args.get("userId");
-        UserReturn user = null;
-//        cacheeditInfoHandler = (CacheEditInfoHandler)this.cacheHandler;
-//        User user = (User) cacheeditInfoHandler.getUserFromCache(ids[0],User.class);
-//        if(user!=null) {
-//            return user;
-//
-//        }
+        String userId = (String) args.get("userId");
+        cacheEditInfoHandler = (CacheEditInfoHandler) this.cacheHandler;
+        UserReturn user = (UserReturn) cacheEditInfoHandler.getUserFromCache(userId, UserReturn.class);
+        if(user != null) {
+            return user;
+        }
         EditInfoHandler dbHandler = (EditInfoHandler) this.dbHandler;
 
-        // get notifications from db
-        if (args.containsKey("profileId"))
-            user = dbHandler.getUserProfile((String) args.get("profileId"));
-        else
-            user = dbHandler.getUserProfile((String) args.get("userId"));
+        user = dbHandler.getUserProfile((String) args.get("userId"));
 
-        ArrayList<UserReturn> users = new ArrayList<UserReturn>();
-        users.add(user);
-//        cacheeditInfoHandler.saveUsersInCache(ids,users);
+        cacheEditInfoHandler.saveUsersInCache(userId, user);
         return user;
     }
 }
